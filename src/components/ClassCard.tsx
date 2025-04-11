8075d26ecd73bb2fbe7074dacaea52611bdba134
Armin <armin2003sheibak@gmail.com>
2025-04-11 17:33:38 +0330

    fix the class card bug

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ClassCard = ({name, description, image}: Props) => {
   return (
     <li className="relative mx-5 inline-block h-[380px] w-[450px]" >
-      <div className="p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90" >
+      <div className="p-5 absolute z-5 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90" >
         <p  className="text-2xl mb-5">{name}</p>
         <p>{description}</p>
       </div>
